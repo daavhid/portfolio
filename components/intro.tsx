d43d19a1592a82e909c0b5221bb64f7bc8013746
daavhid <victortoyosi06@gmail.com>
2025-01-24 14:00:55 +0100

    changed the text

--- components/intro.tsx
+++ components/intro.tsx
@@ -56,7 +56,7 @@ const Intro = () => {
         className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'>
             <span className='font-bold'>Hello, I'm David Oluwatoyosi</span>,
             I'm a {" "}<span className='font-bold'>full-stack developer</span> {" "}
-            with <span className='font-bold'>5 years</span> of experience. I enjoy building <span className='italic'>sites & apps</span>.{" "}
+            with <span className='font-bold'>3+ years</span> of experience. I enjoy building <span className='italic'>sites & apps</span>.{" "}
             My focus is <span className='underline'>React(Next.js)</span>.
         </motion.h1>
         <motion.div
